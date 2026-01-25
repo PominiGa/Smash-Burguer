5add806673435774a76da5cc1aea2db7ccf28ef8
PominiGa <gabriel.souza53@hotmail.com>
2026-01-25 01:30:51 -0300

    first commit

---
+++ src/test/java/com/example/Smash/SmashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Smash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
